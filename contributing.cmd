6fc9b1789ececbbeddd02d1b63bd050c91d89921
MrYanez <129912196+MrYanez@users.noreply.github.com>
2023-06-06 18:49:13 -0700

    Update contributing.cmd

--- contributing.cmd
+++ contributing.cmd
@@ -1,1 +1,40 @@
 
+<h2>Welcome</h2>
+Welcome to the world of Artificial Intelligence! This is an open source project created and maintained by Group 1 which includes Nahum, Bennett, and Celeste in IT115. In this guide you will get an overview of the contribution workflow. The goal of our project is to learn our way around GitHub, learn a bunch of coding and teach about Artificial Intelligence.
+
+<h2>Introduction</h2>
+<p>This webpage is focused in understanding and documentaing about the world of artificial intelligence</p>
+<p>Topics will be enlisted histocally speaking as its evolution for the artificial intelligence</p>
+
+<h2>Team Members</h2>
+Nahum
+<br>
+Bennett
+<br>
+Celeste
+
+<h2>Testing</h2>
+<p>Make sure changes are applied to main</p>
+
+<h2>Bug Reports</h2>
+If you run into any bugs while using a demo of the main branch, please proceed with these steps:
+<br> 
+1. Create a new issue in the issues tab located at the top
+<br>2. 
+Explain the behavior of the website that is not working properly.
+<br>3. 
+Provide as much detail and context in the comment that you can before posting (i.e. how to reproduce the bug, where the bug happens, etc.)
+<br>4. 
+Leave a comment about the issue. 
+
+<h2>Code of Conduct</h2>
+<!--Next lines dded by Nahum--!>
+<p>Examples of encouraged behavior that contributes to a positive environment include:</p>
+<ul> 
+    <li>Using welcoming and inclusive language</li>
+    <li>Being respectful of differing viewpoints and experiences</li>
+    <li>Gracefully accepting constructive criticism</li>
+    <li>Focusing on what is best for everyone at the event</li>
+    <li>Showing empathy towards other participants</li>
+    </ul>
+    
